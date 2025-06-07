d5d123d5ad5d38d1197ac0d260c333bf8553b7aa
Duc <pnhd0109@gmail.com>
2025-06-07 16:11:13 +0700

    Add project files.

---
+++ CoffeeShop/Program.cs
@@ -0,0 +1,51 @@
+using CoffeeShop.Data;
+using CoffeeShop.Models.Interfaces;
+using CoffeeShop.Models.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>(ShoppingCartRepository.GetCart);
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+builder.Services.AddDbContext<CoffeeshopDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeShopDbContextConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<CoffeeshopDbContext>();
+
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorPages();
+
+
+var app = builder.Build();
+app.UseSession();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.MapRazorPages();
+
+app.UseRouting();
+app.UseAuthentication();
+
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+
